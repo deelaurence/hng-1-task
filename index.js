81d5d8684f6a6f5a921d13da117c8793a8d1c6c5
deelaurence <laurie143pelumi@gmail.com>
2022-10-28 13:43:31 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors =require ('cors')
+const app = express()
+app.use(cors('*'))
+const port = process.env.PORT||4000
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`);
+})
+app.get('/',(req,res)=>{
+    res.json({
+        slackUsername:"deverence",
+        backend:"true",
+        age:27,
+        bio:"a fullstack MERN stack web developer trying to build more node.js muscle"
+    })
+})
